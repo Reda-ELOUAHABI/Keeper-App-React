49d9d9130978150cd7584049d82ab0a520ef35e8
reda-coder <59047199+reda-coder@users.noreply.github.com>
2021-03-26 21:55:33 +0100

    Initial commit

---
+++ src/components/App.js
@@ -0,0 +1,41 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+import notes from "../notes";
+//     c'est tres interesant !
+function App() {
+  return (
+    <div>
+      <Header />
+
+      {notes.map((noteItem) => (
+        <Note
+          key={noteItem.key}
+          title={noteItem.title}
+          content={noteItem.content}
+        />
+      ))}
+      <Footer />
+    </div>
+  );
+}
+
+// function App() {
+//   return (
+//     <div>
+//       <Header />
+//       {notes.map(noteItem => (
+//         <Note
+//           key={noteItem.key}
+//           title={noteItem.title}
+//           content={noteItem.content}
+//         />
+//       ))}
+//       <Footer />
+//     </div>
+//   );
+// }
+
+export default App;
+//don't forget each time to export your file
